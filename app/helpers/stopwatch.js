5e82ee559b740c4817c3a6786461812d6f3e4dbb
Daniel Liburd <daniel.liburd@ticketmaster.co.uk>
2017-02-10 15:19:39 +0000

    use string interpolation for helper function

--- app/helpers/stopwatch.js
+++ app/helpers/stopwatch.js
@@ -2,5 +2,5 @@ export function _formatTime (secondsElapsed) {
   const minute = Math.floor(secondsElapsed / 60)
   const seconds = ('0' + secondsElapsed % 60).slice(-2)
 
-  return minute + ':' + seconds
+  return `${minute} : ${seconds}`
 }
